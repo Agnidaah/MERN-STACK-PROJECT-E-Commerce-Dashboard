faef614fd31a88940075f000353938089353723c
Agnidaah <108724695+Agnidaah@users.noreply.github.com>
2023-02-15 15:44:39 +0530

    commit

---
+++ E-DASHBOARD/index.js
@@ -0,0 +1,70 @@
+const express=require('express');
+const cors=require("cors");
+require('./db/config');
+const User=require('./db/User');
+const Product=require("./db/Products");
+const app=express();
+app.use(express.json());
+app.use(cors());
+app.post("/register",async (req,res)=>{
+    const user=new User(req.body);
+    let result=await user.save();
+    result=result.toObject();
+    delete result.password;
+    res.send(result);
+})
+app.post("/login",async (req,res)=>{
+    //let user=await User.findOne(req.body).select("-password");//you can use this also
+    if(req.body.password && req.body.email){
+        let user=await User.findOne(req.body,{}, { projection: { password:0} });
+    if(user){
+        res.send(user);
+    }else{
+        res.send({result:"No User Found"});
+    }
+    }else{
+        res.send({result:"No User Found"});
+    }
+})
+app.post("/add-product",async (req,res)=>{
+    let product=new Product(req.body);
+    let result=await product.save();
+    res.send(result);
+})
+app.get("/products",async (req,res)=>{
+    let products=await Product.find();
+    if(products.length>0){
+        res.send(products);
+    }else{
+        res.send({result:"No Products Found"});
+    }
+})
+app.delete("/product/:id",async (req,res)=>{
+    const result=await Product.deleteOne({_id:req.params.id});
+    res.send(result);
+})
+app.get("/product/:id",async (req,res)=>{
+    let result=await Product.findOne({_id:req.params.id});
+    if(result){
+    res.send(result);}
+    else{
+        res.send({result:"Product not found"});
+    }
+})
+app.put("/product/:id",async(req,res)=>{
+    let result=await Product.updateOne(
+        {_id:req.params.id },
+        {$set:req.body}) 
+        res.send(result);
+});
+app.get("/search/:key",async (req,res)=>{
+    let result = await Product.find({
+        "$or":[
+            {name:{$regex:req.params.key}},
+            {company:{$regex:req.params.key}},
+            {category:{$regex:req.params.key}}
+        ]
+    });
+    res.send(result);
+})
+app.listen(5000);
